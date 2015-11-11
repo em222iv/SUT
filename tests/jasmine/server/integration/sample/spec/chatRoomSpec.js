0f15568078f7be9f18f88974c9724f0b7b52533e
em222iv <erikper.magnusson@gmail.com>
2015-11-11 10:35:07 +0100

    RED

--- tests/jasmine/server/integration/sample/spec/chatRoomSpec.js
+++ tests/jasmine/server/integration/sample/spec/chatRoomSpec.js
@@ -1,7 +1,18 @@
 describe("Rooms", function() {
 
+    beforeEach(function() {
+        this.name = "chat Room 1"
+        this.room = new Room(this.name);
+    });
+
+    it("should take name via constructor", function() {
+        expect(this.room.name).toEqual(this.name);
+    });
+
     it("should exist a Rooms collection", function() {
         expect(Rooms);
     });
+
+
 
 });
